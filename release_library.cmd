10601e3dfc9f0246eeec91f75a40319ea7f5bb18
星翼 <853974536@qq.com>
2017-06-27 10:47:22 +0800

    Update release script.

--- release_library.cmd
+++ release_library.cmd
@@ -4,7 +4,7 @@ md SDLClasses_ReleasePack\lib
 md SDLClasses_ReleasePack\lib\x86
 md SDLClasses_ReleasePack\lib\x64
 
-copy include\*.* SDLClasses_ReleasePack\include\*.* /f /s /q
+copy include\*.* SDLClasses_ReleasePack\include\*.*
 copy SDLClasses\Release\SDLClasses.lib SDLClasses_ReleasePack\lib\x86\SDLClasses.lib
 copy SDLClasses\x64\Release\SDLClasses.lib SDLClasses_ReleasePack\lib\x64\SDLClasses.lib
 copy LICENSE SDLClasses_ReleasePack\LICENSE
