cadfdbaf3f0935c5bfd284a28944f23678e7f554
星翼 <853974536@qq.com>
2017-06-27 10:46:23 +0800

    Update a script to release library packet.

---
+++ release_library.cmd
@@ -0,0 +1,11 @@
+md SDLClasses_ReleasePack
+md SDLClasses_ReleasePack\include
+md SDLClasses_ReleasePack\lib
+md SDLClasses_ReleasePack\lib\x86
+md SDLClasses_ReleasePack\lib\x64
+
+copy include\*.* SDLClasses_ReleasePack\include\*.* /f /s /q
+copy SDLClasses\Release\SDLClasses.lib SDLClasses_ReleasePack\lib\x86\SDLClasses.lib
+copy SDLClasses\x64\Release\SDLClasses.lib SDLClasses_ReleasePack\lib\x64\SDLClasses.lib
+copy LICENSE SDLClasses_ReleasePack\LICENSE
+
