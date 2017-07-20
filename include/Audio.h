559628bf7929948d472efe91f26ff7fe5625a33f
星翼 <853974536@qq.com>
2017-07-20 19:54:42 +0800

    Audio Class added.

---
+++ include/Audio.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Handler.h"
+/* Comming soon
+namespace SDL 
+{
+
+	struct AudioSpec
+	{
+
+	};
+
+	class Audio
+	{
+	public:
+		Audio();
+		~Audio();
+
+	private:
+		Handler audioDevice_;
+	};
+}*/
