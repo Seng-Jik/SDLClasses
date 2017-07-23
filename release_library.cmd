1dfe1898cbcaa4527d18d05fa878c813ab1fc6a9
星翼 <853974536@qq.com>
2017-07-23 20:08:10 +0800

    Bug fixed.

--- release_library.cmd
+++ release_library.cmd
@@ -1,11 +1,15 @@
 md SDLClasses_ReleasePack
 md SDLClasses_ReleasePack\include
 md SDLClasses_ReleasePack\lib
-md SDLClasses_ReleasePack\lib\x86
-md SDLClasses_ReleasePack\lib\x64
+md SDLClasses_ReleasePack\lib\Win32Debug
+md SDLClasses_ReleasePack\lib\x64Debug
+md SDLClasses_ReleasePack\lib\Win32Release
+md SDLClasses_ReleasePack\lib\x64Release
 
 copy include\*.* SDLClasses_ReleasePack\include\*.*
-copy SDLClasses\Release\SDLClasses.lib SDLClasses_ReleasePack\lib\x86\SDLClasses.lib
-copy SDLClasses\x64\Release\SDLClasses.lib SDLClasses_ReleasePack\lib\x64\SDLClasses.lib
+copy SDLClasses\Release\SDLClasses.lib SDLClasses_ReleasePack\lib\Win32Release\SDLClasses.lib
+copy SDLClasses\x64\Release\SDLClasses.lib SDLClasses_ReleasePack\lib\x64Release\SDLClasses.lib
+copy SDLClasses\Debug\SDLClasses.lib SDLClasses_ReleasePack\lib\Win32Debug\SDLClasses.lib
+copy SDLClasses\x64\Debug\SDLClasses.lib SDLClasses_ReleasePack\lib\x64Debug\SDLClasses.lib
 copy LICENSE SDLClasses_ReleasePack\LICENSE
 
